c5abdfe10e64dfe618ba26779d7fccebc4a6ba9f
holly_sizzle <121349605+hollySizzle@users.noreply.github.com>
2023-05-21 14:27:09 +0900

    Update add_admin_user.sh

--- add_admin_user.sh
+++ add_admin_user.sh
@@ -21,14 +21,14 @@ read -p "sshの公開鍵を入力してください " ssh_open_key
 sudo cd /home/$username
 
 # ~/.ssh/authorized_keysファイルに公開鍵の内容を追記します
-sudo mkdir .ssh || echo ".ssh directory already exists"
+sudo mkdir /home/$username/.ssh || echo ".ssh directory already exists"
 sudo chown -R $username:$username .ssh
-sudo echo $ssh_open_key | tee -a .ssh/authorized_keys || echo 'authorized_keysの操作に失敗しました'
-sudo chown -R $username:$username .ssh
+sudo echo $ssh_open_key | tee -a /home/$username/.ssh/authorized_keys || echo 'authorized_keysの操作に失敗しました'
+sudo chown -R $username:$username /home/$username/.ssh
 
 # 所有者以外のアクセスを許可しないようにパーミッションを変更します
-sudo chmod 700 .ssh/ || echo '.sshの権限変更に失敗しました'
-sudo chmod 600 .ssh/authorized_keys || echo '.ssh/authorized_keysの権限変更に失敗しました'
+sudo chmod 700 /home/$username/.ssh/ || echo '.sshの権限変更に失敗しました'
+sudo chmod 600 /home/$username/.ssh/authorized_keys || echo '.ssh/authorized_keysの権限変更に失敗しました'
 
 # ユーザーをdockerグループに追加
 sudo usermod -aG docker $username || echo 'ユーザーをdockerグループに追加出来ません'
